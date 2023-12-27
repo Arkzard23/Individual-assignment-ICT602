9f890bcfb85fbc5018462c1eb69e8934346d05b3
Arkzard23 <154819529+Arkzard23@users.noreply.github.com>
2023-12-27 23:22:33 +0800

    SplashActivity.java

---
+++ SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.individualassignment2;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private static final long SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
